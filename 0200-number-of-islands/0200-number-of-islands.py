39e8a62b411bb96a999bd97788f9873b024cbcd6
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-08-04 19:13:29 -0400

    Time: 290 ms (22.83%), Space: 18.9 MB (57.51%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -7,44 +7,35 @@ class Solution:
         n = len(grid[0])
 
         visited = [ [0] * n for _ in range(m)]
+        def bfs(r,c):
+            q = deque()
 
+            q.append([r,c])
+            visited[r][c] = 1
+            while q:
+                i, j = q.popleft()
+                visited[i][j] = 1
+                # print("visited ",i,j)
+
+                neighbors = [(i+1,j),
+                                (i-1,j),
+                                (i,j+1),
+                                (i,j-1)]
+                
+                for n_r,n_c in neighbors:
+                    if (n_r in range(m) and
+                        n_c in range(n) and
+                        visited[n_r][n_c] == 0 and 
+                        grid[n_r][n_c] =="1"):
+                        q.append([n_r,n_c])
+                        visited[n_r][n_c] = 1
         for r in range(m):
             for c in range(n):
-                # print("r,c =",r,c)
-                # print(visited[r][c],grid[r][c])
                 if visited[r][c] == 0 and grid[r][c] == "1":
-                    # print(visited)
                     n_island += 1
-                    # print('Count> r,c =',r,c)
-
-                    #bfs traversal
-
-                    q = deque()
-
-                    q.append([r,c])
-                    visited[r][c] = 1
-                    while q:
-                        i, j = q.popleft()
-                        visited[i][j] = 1
-                        # print("visited ",i,j)
-
-                        neighbors = [(i+1,j),
-                                     (i-1,j),
-                                     (i,j+1),
-                                     (i,j-1)]
-                        
-                        for n_r,n_c in neighbors:
-                            if (n_r in range(m) and
-                                n_c in range(n) and
-                                visited[n_r][n_c] == 0 and 
-                                grid[n_r][n_c] =="1"):
-                                q.append([n_r,n_c])
-                                visited[n_r][n_c] = 1
-                        
-
+                    bfs(r,c)
                 elif visited[r][c] == 0 and grid[r][c] == "0":
                     visited[r][c] = 1
-                    # print("visited ",r,c)
                 
         
         return n_island
