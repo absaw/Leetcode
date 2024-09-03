cfbae2e67f77bfa84fddcaa8080459b8d48263d6
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-09-02 20:01:06 -0400

    Time: 43 ms (53.35%), Space: 16.7 MB (36.04%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,13 +1,20 @@
-from collections import defaultdict
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        d,e = defaultdict(),defaultdict()
+        
+        if len(s) != len(t):
+            return False
 
-        for i,j in zip(s,t):
-            if ((i in d.keys() and d[i] != j) or
-                (j in e.keys() and e[j] != i)):
-                return False
-            d[i] = j
-            e[j] = i
+        d = {}
 
+        for i,c in enumerate(s):
+            
+            if c not in d:
+                if t[i] in d.values():
+                    return False
+                d[c] = t[i]
+            
+            else:
+                if d[c] != t[i]:
+                    return False
+        
         return True
