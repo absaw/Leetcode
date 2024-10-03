bd883507977eb30760e8243dcdf1657288e75b2d
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-10-03 13:38:13 -0400

    Time: 94 ms (29.14%), Space: 22 MB (25.91%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -7,10 +7,10 @@ class Solution:
         # print(freq_list)
         for n in nums:
             count_dict[n] = 1 + count_dict.get(n,0)
-        print(count_dict)
+        # print(count_dict)
         for key,val in count_dict.items():
             freq_list[val].append(key)
-        print(freq_list)
+        # print(freq_list)
         res = []
         res_ele = 0
         for count_val in range(len(freq_list)-1,0,-1):
