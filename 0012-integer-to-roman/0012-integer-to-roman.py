d41d22fda12d86436487378d164fa90e54099c79
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-10-19 12:11:18 -0400

    Time: 4 ms (100%), Space: 16.8 MB (6.12%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,25 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        symList = [
+            ["I", 1],
+            ["IV", 4],
+            ["V", 5],
+            ["IX", 9],
+            ["X", 10],
+            ["XL", 40],
+            ["L", 50],
+            ["XC", 90],
+            ["C", 100],
+            ["CD", 400],
+            ["D", 500],
+            ["CM", 900],
+            ["M", 1000],
+        ]
+        res = ""
+        for symbol,val in reversed(symList):
+            if num//val:
+                digit = num//val
+                res += symbol*digit
+                num = num % val
+        
+        return res
