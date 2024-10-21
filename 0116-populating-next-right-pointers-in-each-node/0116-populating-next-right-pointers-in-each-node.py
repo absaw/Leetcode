f766152b3c62b4dcbb08fb965ef01432ab12d520
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-10-21 16:49:25 -0400

    Time: 44 ms (81.06%), Space: 18 MB (80.69%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -7,36 +7,22 @@ class Node:
         self.right = right
         self.next = next
 """
-from collections import deque
+
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        
-        # level order traversal
-        # add root to deque
-        # while not queue
-        # popleft from queue to node
-        # if queue is empty set right to none
-        # else set next to first element in queue if in same level. if not same level set to none
-        # add children to queue with level+1
-        if not root:
-            return None
-        head = root
-        queue = deque()
-        level = 0
-        queue.append([root,level])
-        while(queue):
-            node, level = queue.popleft()
 
-            # if not queue:
-            #     node.right = None #not needed
-            # else:
-            if queue:
-                nextElement,nextLevel = queue[0]
-                if nextLevel == level:
-                    node.next = nextElement
-            for child in [node.left,node.right]:
-                if child:
-                    queue.append([child,level+1])
+        current = root
+        nxt = root.left if root else None
+
+        while current and nxt:
+            current.left.next = current.right
+            # nxt.next = current.right
+            if current.next:
+                current.right.next = current.next.left
+            
+            current = current.next
+            if not current:
+                current = nxt
+                nxt = current.left
         
-        return head
-                
+        return root
