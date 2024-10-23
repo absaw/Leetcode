ecac098722d1a932a4d06a12934ed340cf58f9d1
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-10-23 18:55:54 -0400

    Time: 0 ms (100%), Space: 17.5 MB (98.03%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -3,17 +3,31 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+'''
+  p c>n
+    1 2 3 4 5 N
+  p<  c n
+    1 2 3 4 5 N
+save n=c.n 
+c.n = p
+p=c
+c = n
+n = n.n
+
+'''
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
         prev = None
+        curr = head
+        # nxt = head.next
 
-        current = head
-        while current:
-            next_node = current.next
-            current.next = prev
-            
-            prev = current
-            current = next_node
-        
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+            # nxt = nxt.next
         return prev
