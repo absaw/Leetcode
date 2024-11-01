868be7076bb3c39064d36ce6e41dbc28d12d2b23
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-11-01 12:40:57 -0400

    Time: 111 ms (13.58%), Space: 22.1 MB (80.56%) - LeetHub

---
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -0,0 +1,16 @@
+import heapq, math
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        maxHeap = []
+        for x,y in points:
+            distance = math.sqrt(x**2+y**2)
+            maxHeap.append([-distance,(x,y)])
+
+        heapq.heapify(maxHeap)
+        res = []
+        while len(maxHeap)!=0:
+            if len(maxHeap) > k:
+                heapq.heappop(maxHeap)
+            else:
+                res.append(heapq.heappop(maxHeap)[1])
+        return res
