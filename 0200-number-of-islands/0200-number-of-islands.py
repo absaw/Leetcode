bf63cf1d589157d2bb50847756e8741449556005
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-11-08 19:02:45 -0500

    Time: 274 ms (24.39%), Space: 19.1 MB (46.48%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,38 +1,27 @@
-from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
-        n_island = 0
+        visited = set()
         m = len(grid)
         n = len(grid[0])
+        def dfs(r,c):
+            
+            if (r not in range(m) or
+                c not in range(n) or 
+                # (r,c) in visited or 
+                grid[r][c] != "1"):
+                return 
+            
+            grid[r][c] = "3"
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
 
-        visited = [ [0] * n for _ in range(m)]
-        def bfs(r,c):
-            q = deque()
-            q.append([r,c])
-            visited[r][c] = 1
-            while q:
-                i, j = q.popleft()
-                visited[i][j] = 1
-                neighbors = [(i+1,j),
-                                (i-1,j),
-                                (i,j+1),
-                                (i,j-1)]
-                
-                for n_r,n_c in neighbors:
-                    if (n_r in range(m) and
-                        n_c in range(n) and
-                        visited[n_r][n_c] == 0 and 
-                        grid[n_r][n_c] =="1"):
-                        q.append([n_r,n_c])
-                        visited[n_r][n_c] = 1 # This makes all the difference since we aren't visting each neighbor now. 
+        nIslands = 0
         for r in range(m):
             for c in range(n):
-                if visited[r][c] == 0 and grid[r][c] == "1":
-                    n_island += 1
-                    bfs(r,c)
-                elif visited[r][c] == 0 and grid[r][c] == "0":
-                    visited[r][c] = 1
-                
-        
-        return n_island
+                if grid[r][c] == "1":
+                    dfs(r,c)
+                    nIslands += 1
+                    
+        return nIslands
