51992efa9610e59b3389202e5f986b1b74d4243a
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-11-12 20:45:42 -0500

    Time: 46 ms (17.27%), Space: 17.1 MB (23.5%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -4,25 +4,35 @@ class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
+
+Create a dictionary which stores the cloned new nodes
+Perform a dfs from the current node
+When a new node is seen, create a clone and store
+When its already seen, get it and append it to the neighbor
+      v
+1-2-3-4
+| |\
+5 6 7
+
+1n 2n 3n-4n
+1:1n
+2:2n
+3:3n
+4: 4n
 """
 
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        
-
-        old_to_new = {}
-
-        def get_node(node):
-            if node in old_to_new:
-                return old_to_new[node]
-            if not node:
-                return None
-            new_node = Node(node.val,None)
-            old_to_new[node] = new_node
-            for neighbor in node.neighbors:
-                new_node.neighbors.append(get_node(neighbor))
+        nodeDict = {}
+        def dfs(node):
+            if node in nodeDict:
+                return nodeDict[node]
             
-            return new_node
-        
-        return get_node(node) 
+            newNode = Node(node.val)
+            nodeDict[node] = newNode
+            for neighbor in node.neighbors:
+                newNode.neighbors.append(dfs(neighbor))
+            return newNode
+
+        return dfs(node) if node else None
