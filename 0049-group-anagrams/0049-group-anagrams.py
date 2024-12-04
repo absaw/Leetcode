cb3c02eafd9ad801a7e72920f831df13c074db76
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-12-04 17:25:37 -0500

    Time: 23 ms (20.06%), Space: 22 MB (11.55%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,21 +1,16 @@
-'''
-idx_anagram_array = [0]*26
-for str in strs:
-for c in str:
-    i = ord('c')-ord('a')
-    idx_anagram_array[i] += 1
-dict[tuple(idx_anagram_array)]+=1
-return dict.values() 
-'''
-from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dct = defaultdict(list)
-        for s in strs:
-            idx_anagram_array = [0]*26
-            for c in s:
-                i = ord(c)-ord('a')
-                idx_anagram_array[i] += 1
-            dct[tuple(idx_anagram_array)].append(s)
-        # print(list(dct.values()))
-        return list(dct.values())
+        anaD = {}
+
+        for word in strs:
+            wordKey = [0]*26
+            for ch in word:
+                i = ord(ch) - ord('a')
+                wordKey[i] += 1
+            t = tuple(wordKey)
+            if t in anaD:
+                anaD[t].append(word)
+            else:
+                anaD[t] = [word]
+        
+        return list(anaD.values())
