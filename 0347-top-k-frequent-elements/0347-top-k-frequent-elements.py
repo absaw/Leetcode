e62d9fac23e6b9764e46fc5ae353ef470eabc578
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-12-04 19:10:12 -0500

    Time: 4 ms (71.5%), Space: 20.9 MB (42.78%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,19 +1,40 @@
+'''
+[4,5,6,7,4,1,2,4,1,1,1,2,]
+
+top k = 2
+1:4
+2: 2
+4: 3
+5: 1
+6: 1
+7:1
+minheap of length = k
+
+minHeap([freq,num])
+if currFreq > minHeap[-1][0]:
+    heapq.heappushpop(minHeap,[currFreq,num])
+else:
+    heapq.heappop()
+'''
 import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        freq = {}
+
+        for num in nums:
+            freq[num] = 1 + freq.get(num,0)
+        #freq = {number 1: frequency 4, 2: 3, 3:5}
         minHeap = []
-        countDict = {}
-
-        for number in nums:
-            countDict[number] = 1 + countDict.get(number,0)
-
-        for num, freq in countDict.items():
-            heapq.heappush(minHeap,(freq,num))
-            
+        heapq.heapify(minHeap)
+        for num,freq in freq.items():
+            # if len(minHeap)<k:
+            heapq.heappush(minHeap,[freq,num])
             if len(minHeap)>k:
                 heapq.heappop(minHeap)
-
+            
         result = []
-        for i in range(len(minHeap)):
+        i = 0
+        while i<k and i < len(minHeap):
             result.append(heapq.heappop(minHeap)[1])
         return result
