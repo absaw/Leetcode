9d76cda5bcc73986cd67936a0a40c9650e037940
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-12-11 21:25:40 -0500

    Time: 123 ms (29.93%), Space: 17.5 MB (15.6%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,36 +1,18 @@
-'''
-ABCAAADA
- ^^
-k = 1
-no. of replacements needed = winLength - max frequency character in all
-for eg.
-AAAAABAB
-L      R
-A = 5
-B = 1
-k=3
-nreplacements = 8-6 = 2
-so 2 Bs can be replaced
-'''
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        chArr = [0]*26
         l = r = 0
-        lSub = 0
+        maxF = 0
+        chArr = [0]*26
+        lsub = 0
         while r < len(s):
             winLen = r-l+1
-            ich = ord(s[r])-ord('A')
-            chArr[ich]+=1
-            mfchar = max(chArr)
-            
-            nReplacement = winLen - mfchar
-            if nReplacement <= k:
-                lSub = max(lSub,winLen)
+            chArr[ord(s[r])-ord('A')] += 1
+            maxF = max(maxF, chArr[ord(s[r])-ord('A')])
+
+            if winLen-maxF > k:
+                chArr[ord(s[l])-ord('A')] -= 1
+                l+=1
             else:
-                i_ch = ord(s[l])-ord('A')
-                chArr[i_ch]-=1
-                l += 1
-                
-            r +=1
-        return lSub
-                
+                lsub = max(lsub,winLen)
+            r += 1
+        return lsub
