896bd52e2f112afe234f72a718088942e016a3e7
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-12-14 13:31:15 -0500

    Time: 735 ms (26.71%), Space: 24 MB (67.59%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -1,39 +1,41 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         
-        # merge sort
-        def merge(a,l,m, r):
-            L = a[l:m+1]
-            R = a[m+1:r+1]
+        def merge_sort(arr, l, r):
+            #base case when only  1element, no need to sort it
+            if l == r:
+                return
+            
+            mid = (l + r) // 2
+            merge_sort(arr,l,mid)
+            merge_sort(arr,mid+1,r)
+            merge(arr,l,mid,r)
+
+        def merge(arr, l, mid, r):
+            #[2,3,4,5]
+            # i
+            #[-2,-1,0,1]
+            #  j
+            left = arr[l:mid+1]
+            right = arr[mid+1:r+1]
             i = j = 0
             k = l
 
-            while i < len(L) and j < len(R):
-                if L[i] < R[j]:
-                    a[k] = L[i]
-                    i+=1
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                    arr[k] = left[i]
+                    i += 1
                 else:
-                    a[k] = R[j]
+                    arr[k] = right[j]
                     j += 1
                 k += 1
-            while i < len(L):
-                a[k] = L[i]
+            while i < len(left):
+                arr[k] = left[i]
                 i += 1
                 k += 1
-            while j < len(R):
-                a[k] = R[j]
-                k += 1
+            while j < len(right):
+                arr[k] = right[j]
                 j += 1
-
-        def merge_sort(a, l, r):
-
-            if l==r:
-                return
-            mid = (l+r)//2
-            merge_sort(a,l,mid)
-            merge_sort(a,mid+1,r)
-            merge(a,l,mid,r)
-        
-        merge_sort(nums,0,len(nums)-1)
-
+                k += 1
+        merge_sort(nums,0,len(nums))
         return nums
