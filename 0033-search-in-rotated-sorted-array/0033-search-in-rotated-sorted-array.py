7ed77038721693a98d5bfd29661efff6c471d6f1
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-12-19 19:55:26 -0500

    Time: 0 ms (100%), Space: 18.2 MB (10.12%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,23 +1,52 @@
+'''
+4, 5, 6, 7, 0, 1, 2
+l
+                  r
+         m
+
+target = 5
+
+while l<=r:
+
+    calc mid
+    if nums[mid]>nums[l]:
+        if target < nums[mid]:
+            move right
+        else:
+            move left
+    elif nums[mid]<nums[l]:
+        if target>nums[mid]:
+            move right
+        else: 
+            move left
+
+'''
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
         l = 0
         r = len(nums)-1
-
+        ans = -1
         while l <= r:
-            mid = (l+r)//2
-            if nums[mid] ==target:
-                return mid
-            #left sorted portion
-            if nums[l] <= nums[mid]:
-                if nums[l] <= target < nums[mid]:
+# 4, 5, 6, 7, 0, 1, 2
+# l
+#                   r
+#           m
+            mid = l + (r-l)//2
+            if nums[mid] == target:
+                ans = mid
+                break
+            if nums[mid] >= nums[l]:
+                # large sorted section
+                if target in range(nums[l],nums[mid]+1):
                     r = mid-1
                 else:
                     l = mid+1
-            #right sorted
             else:
-                if nums[mid] < target <= nums[r]:
+                #small sorted section
+                if target in range(nums[mid],nums[r]+1):
                     l = mid+1
                 else:
                     r = mid-1
-        return -1
+        return ans
+
+
