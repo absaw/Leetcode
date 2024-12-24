b48db651410c070e2b94cc8c79cd2eb7e8110d27
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-12-24 17:11:51 -0500

    Time: 3 ms (14.51%), Space: 17.6 MB (8.61%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,22 +5,36 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        '''
+        merging 2 lists
+        better to have a dummy node at the beginning to represent the output node
+        pick the smaller node at each iteration
+        When one of the lists are done, just add the other list's node to the end and the 
+        result is formed 
+               op  
+                  p1
+    d - 1 - 2 - 4
+    
+        5 > 6 > 7
+        p2
+        '''
         dummy = ListNode()
-        head = dummy
+        ptr_output = dummy
+        ptr1 = list1
+        ptr2 = list2
 
-        p = list1
-        q = list2
-
-        while p or q:
-            val1 = p.val if p else float('inf')
-            val2 = q.val if q else float('inf')
-            if val1 < val2:
-                nxt = p.next
-                dummy.next = p
-                p = nxt
+        while ptr1 and ptr2:
+            if ptr1.val <= ptr2.val:
+                ptr_output.next = ptr1
+                ptr1 = ptr1.next
             else:
-                nxt = q.next
-                dummy.next = q
-                q = nxt
-            dummy = dummy.next
-        return head.next
+                ptr_output.next = ptr2
+                ptr2 = ptr2.next
+            ptr_output = ptr_output.next
+        if not ptr1:
+            ptr_output.next = ptr2
+        else:
+            ptr_output.next = ptr1
+        
+        return dummy.next
