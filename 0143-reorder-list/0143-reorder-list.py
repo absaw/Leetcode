9d961b982f19ebe9507b81fda493b440ba12de45
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-12-24 18:21:20 -0500

    Time: 0 ms (100%), Space: 23.4 MB (8.66%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -3,54 +3,55 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-'''
-head 
-1 > 2 > 3 > 4 > 5 > 6 > None
-        m
-1 > 6 > 2 > 5 > 3 > 4 > None
-
-Find halfway point of list
-set next to null
-reverse 2nd linked list 
-merge the the list alternately
-'''
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
-        #halfway point
-        dummy = ListNode()
+        # 1 > 2 > 3 > 4 > None
+        #     s
+        #              f   
+        
+        # 1 > 2 > 3 > 4 > 5
+        
+        # find the mid of list
         slow = head
         fast = head.next
-        # dummy.next = slow
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-            # dummy = dummy.next
-        head2 = slow.next
+        second_list = slow.next
         slow.next = None
-        #reverse a list
-        # p > q > r
-        prev= None
-        curr = head2
+        # reverse the second part list
+        #    3>4>5>None
+        # p  c nxt
+        prev = None
+        curr = second_list
         while curr:
             nxt = curr.next
             curr.next = prev
             prev = curr
             curr = nxt
-        # head2 = prev
+        head2 = prev
+        # merge the two lists
+        # dummy
+        #  1 > 2
+        # ptr1
+        # 4 > 3
+        # ptr2
+        dummy=ListNode()
+        ptr1 = head
+        ptr2 = head2
+        dummy.next = ptr1
+        while ptr1 and ptr2:
+            ptr1nxt = ptr1.next
+            ptr2nxt = ptr2.next
 
-        #merge them together
-        curr1 = head
-        curr2 = prev
-        while curr2:
-            nxt1 = curr1.next
-            nxt2 = curr2.next
-            curr1.next = curr2
-            curr2.next = nxt1
-            curr1 = nxt1
-            curr2 = nxt2
+            ptr1.next= ptr2
+            ptr2.next = ptr1nxt
+            ptr1 = ptr1nxt
+            ptr2 = ptr2nxt
+        return dummy.next
+
+
         
-        return head
-            
