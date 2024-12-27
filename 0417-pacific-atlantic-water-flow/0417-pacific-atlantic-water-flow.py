6206c5bc48f43b3040169278c1a60b8116275049
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-12-27 10:46:53 -0500

    Time: 63 ms (16.83%), Space: 19.7 MB (6.46%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,39 +1,44 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         
-        m = len(heights)
-        n = len(heights[0])
-        pac = set()
-        atl = set()
-
-        def trav(r,c,visited,prevHeight):
-            #dfs traversal
-            if ( (r,c) in visited or 
-                 r not in range(m) or 
-                 c not in range(n) or
-                 heights[r][c] < prevHeight ):
-                 return
+        #pacific ocean 
+        visited_pacific = set()
+        visited_atlantic = set()
+        M = len(heights)
+        N = len(heights[0])
+        neighbors = [[0,1],[0,-1],[1,0],[-1,0]]
+        def dfs(r,c,visited):
+            if (r not in range(M) or 
+                c not in range(N) or
+                (r,c) in visited):
+                return
             visited.add((r,c))
+            #explore the neighbors
+            for dr, dc in neighbors:
+                nr,nc = dr+r,dc+c
+
+                if (nr in range(M) and 
+                    nc in range(N) and
+                    (nr,nc) not in visited and
+                    heights[nr][nc]>=heights[r][c]):
+                    dfs(nr,nc,visited)
         
-            #traverse neighbors
-            trav(r+1,c,visited,heights[r][c])
-            trav(r-1,c,visited,heights[r][c])
-            trav(r,c+1,visited,heights[r][c])
-            trav(r,c-1,visited,heights[r][c])
-
-
-
-        for r in range(m):
-            trav(r,0,pac,heights[r][0])
-            trav(r,n-1, atl,heights[r][n-1])
-
-        for c in range(n):
-            trav(0,c,pac,heights[0][c])
-            trav(m-1,c,atl,heights[m-1][c])
-        result=[]
-        for r in range(m):
-            for c in range(n):
-                if (r,c) in pac and (r,c) in atl:
-                    result.append([r,c])
-        
+        for c in range(N):
+            #pacific row
+            dfs(0,c,visited_pacific)
+            #atlantic row
+            dfs(M-1,c,visited_atlantic)
+        for r in range(M):
+            #pacific col
+            dfs(r,0,visited_pacific)
+            #atlantic col
+            dfs(r,N-1,visited_atlantic)
+        # print(visited_pacific)
+        # print(visited_atlantic)
+        ints = visited_pacific & visited_atlantic
+        # print(ints)
+        result = [[r,c] for r,c in list(ints)]
         return result
+
+
+
