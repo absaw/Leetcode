71f04c517dab27063642e12a13ee8672adc4502a
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-12-26 23:23:25 -0500

    Time: 7 ms (49.58%), Space: 19.9 MB (5.56%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,41 +1,42 @@
-# from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        courseMap = {}
+        adj_list ={}
+
         for course in range(numCourses):
-            courseMap[course] = []
-        for course,pre in prerequisites:
-            courseMap[course].append(pre)
+            adj_list[course] = []
+        # [1,0],[2,3],[3,1]
+        # 0: []
+        # 1: [0]
+        # 2: [3]
+        # 3: [1]
+        # 2 > 3 > 1 > 0
+        for a, b in prerequisites:
+            adj_list[a].append(b)
         
+        #i have to detect if there exists a loop in the graph
+        # if there is a loop, then the dependency can't be fulfilled
+        # for each of the nodes, if there are no neighbors, it can be taken. so no loop
+        # for node with neighbor, all the neighbors must be traversed without any loop
         visited = set()
-
+        rec_stack = set()
         def dfs(course):
-
+            if course in rec_stack:
+                return False
+            # if not adj_list[course]:
+            #     return True
             if course in visited:
-                # print('false1',course)
-                return False
-            
-            if courseMap[course] == []:
                 return True
-        
             visited.add(course)
-
-            for pre in courseMap[course]:
-
-                if not dfs(pre): 
-                    # print('false2',course)
-
-                    return False
+            rec_stack.add(course)
+            can_complete = True
+            for neighbor in adj_list[course]:
+                can_complete = can_complete and dfs(neighbor)
+            rec_stack.remove(course)
+            return can_complete
             
-            visited.remove(course)
-            courseMap[course] =[]
-            return True
-        
+        self.result = True
+
         for course in range(numCourses):
-            if not dfs(course): 
-                # print('false3',course)
-                
-                return False
-        
-        return True
-        
+            # if course not in visited:
+            self.result = self.result and dfs(course)
+        return self.result                
