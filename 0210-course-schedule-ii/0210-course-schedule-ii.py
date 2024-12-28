d4674390f5f9fb7cbc50e18f0cbf2ac92d646b71
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-12-27 22:44:06 -0500

    Time: 3 ms (78.56%), Space: 19.9 MB (5.04%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -6,26 +6,25 @@ class Solution:
             adj_mat[a].append(b)
         self.order_stack = []
         self.visited = set()
-        self.visit_branch =set()
-        def dfs(course):
-            if course in self.visit_branch:
+
+        def dfs(course,visit_branch):
+            if course in visit_branch:
                 return False
-            if course in self.visited:
-                return True
             
-            self.visit_branch.add(course)
+            visit_branch.add(course)
             #neighbors
             for neighbor in adj_mat[course]:
                 if neighbor not in self.visited:
-                    if not dfs(neighbor):
+                    if not dfs(neighbor,visit_branch):
                         return False
-            self.visit_branch.remove(course)
+            visit_branch.remove(course)
             self.visited.add(course)
             self.order_stack.append(course)
             return True
 
         for course in adj_mat:
             if course not in self.visited:
-                if not dfs(course):
+                visit_branch = set()
+                if not dfs(course,visit_branch):
                     return []
         return self.order_stack
