547652c2b264bf62b2d3d326affe331d8f54caa1
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-12-31 14:53:04 -0500

    Time: 0 ms (100%), Space: 17.8 MB (11.7%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,18 +1,13 @@
-'''
-og arr  new arr(having max at each point)
-1       1
-2       2 = max(ignoring 2(=1), considering 2(=2)) 
-3       4 = max(ignoring 3(taking prev val = 2), considering 3(= 3+arr[i-2]))
-1       4 = max(ignoring 1(=4), considering 1(=1+arr[i-2]=1+2=3))
-'''
-
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums)<=2:
-            return max(nums)
-        maxArr = [0]*len(nums)
-        maxArr[0] = nums[0]
-        maxArr[1] = max(nums[1],nums[0])
-        for i in range(2,len(nums)):
-            maxArr[i] = max(maxArr[i-1], nums[i]+maxArr[i-2])
-        return maxArr[-1]
+        
+        rob1 = 0
+        rob2 = 0
+        # [rob1, rob2, i, i+1]
+        for i in range(len(nums)):
+            op1 = nums[i] + rob1
+            op2 = rob2
+            nums[i] = max(op1,op2)
+            rob1 = rob2
+            rob2 = nums[i]
+        return nums[-1]
