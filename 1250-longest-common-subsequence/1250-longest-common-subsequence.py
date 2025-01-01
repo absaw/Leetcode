8d8838f948cd8065f04a9174b7e724683774d36c
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2025-01-01 17:44:24 -0500

    Time: 455 ms (70.24%), Space: 43.5 MB (26.84%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,24 +1,24 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        
-        memo = {} #(ch1,ch2)
-
-        def findMax(ptr1, ptr2):
-            
-            if ptr1 == len(text1) or ptr2 == len(text2):
-                return 0
-            
-            if (ptr1,ptr2) in memo:
-                return memo[(ptr1,ptr2)]
-            else:
-                if text1[ptr1] == text2[ptr2]:
-                    memo[(ptr1,ptr2)] = 1 + findMax(ptr1+1,ptr2+1)
+        '''
+        abcde
+           a   c   e 
+        a  1   1   2
+ t1     b  1   1   2
+        c  1   2   2
+        d  1   2   2
+        e  1   2   3
+        '''
+        l1 = len(text1)
+        l2 = len(text2)
+        memo = [[0 for i in range(l2+1)] for j in range(l1+1)]
+        # print(memo)
+        for r in range(l1-1, -1, -1):
+            for c in range(l2-1, -1, -1):
+                if text1[r] == text2[c]:
+                    memo[r][c] = memo[r+1][c+1] + 1
                 else:
-                    memo[(ptr1,ptr2)] = max(findMax(ptr1+1,ptr2),findMax(ptr1,ptr2+1))
-            
-            return memo[(ptr1,ptr2)]
-
-        findMax(0,0)
-        return memo[(0,0)]
-
+                    memo[r][c] = max(memo[r+1][c], memo[r][c+1])
+                
+        return memo[0][0]
 
