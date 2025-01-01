6f1835732c13718d3586d24560d8b18059de385d
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2025-01-01 02:00:32 -0500

    Time: 7 ms (90.85%), Space: 18.2 MB (24.65%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,25 +1,11 @@
-'''
-Since negative values can be present, each new number can flip the 
-max product. so a good way would be to keep a min and max number 
-which can help us choose which of them should the current number
-be multiplied with to maintain the max of possible values
-        [2, 3, -2, 4]  -1
-currMax  2  6  6   24    48
-currMin  2  2  -12 -48  -48
-res      2  6  6   24    48
-'''
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        
         result = max(nums)
-
-        curMax = 1
-        curMin = 1
+        currMax, currMin = 1, 1
 
         for n in nums:
-            tmp = curMax*n
-            curMax = max(curMax*n, curMin*n, n)
-            curMin = min(tmp, curMin*n, n)
-            result = max(curMax,result)
-
+            tmp = n*currMax
+            currMax = max(n*currMax, n*currMin, n)
+            currMin = min(tmp, n*currMin, n)
+            result = max(result,currMax)
         return result
