866b492d5624ccc3897e7bd74fe39acab5f07994
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2024-12-31 20:21:22 -0500

    Time: 3 ms (29.74%), Space: 17.7 MB (15.65%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,56 +1,47 @@
 '''
-12
-1, 12
-AB, L
-o/p = 2
+1 1 1 0 6
+1,1,
+1,11
 
-226
-2,2,6
-2,26
-22,6
-o/p = 3
-
-06
-
-o/p = 0
-
-11106
-subproblems: at each point, I can take 1 digit(if not 0) or 2 digits(if 1st not 0)
-
-1,1106
- ,1,106     11,06 X
-    10,6     1,06 X
-
-11,106
-  ,10, 6
-
-1.have a dictionary for storing the number of ways for a particular pattern
-recursively divide current string into further parts, such that at each point, I can take 1 digit(if not 0) or 2 digits(if 1st not 0)
-If the current pattern matches prexisting one, just add it 
-
+1, 1
+1, 1, 1,
+1, 1, 1, 0 x
+1, 1, 1, 06 x
+1, 1, 10, 6
+sol = s[i] + s[] 
+min: 1:A
+max: 26: Z, 2 characters
+at each point i have the options
+take 1 character provided its not zero
+take 2 characters, provided it is between 1 and 26
+if the current branch reaches the end with all valid characters, then increase valid count
 '''
-
 
 class Solution:
     def numDecodings(self, s: str) -> int:
         
-        decodeDict = {len(s):1}
-        # 11106
-        def divide(idx):
+        memo = {len(s):1}
+        # s="226"
+        #    ^
+        # ways = 0
+        def dfs(i):
 
-            if idx in decodeDict:
-                return decodeDict[idx]
-            if s[idx]=="0":
+            if i in memo:
+                return memo[i]
+            if i > len(s) or s[i]=="0":
                 return 0
-
-            res = divide(idx+1)
-
-            if idx+1<len(s) and (s[idx]=="1" or s[idx] == "2" and s[idx+1] in "0123456"):
-                res += divide(idx+2)
             
-            decodeDict[idx] = res
-            return res
-        return divide(0)
+            ways = dfs(i+1)
+            if (i<len(s)-1 and 
+                (s[i]=="1" or s[i]=="2" and s[i+1]<'7')):
+                ways += dfs(i+2)
             
+            memo[i] = ways
+            return memo[i]
+        
+        dfs(0)
+        # print(memo)
+        return memo[0] if 0 in memo else 0
 
-            
+
+
