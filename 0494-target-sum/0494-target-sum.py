0a855cd75d669382080c42a739b76a88c2d4e5a7
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2025-01-02 12:54:34 -0500

    Time: 111 ms (73.88%), Space: 18.2 MB (60.63%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,40 +1,16 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         '''
-        [1, 1, 1, 1, 1]
-         i
-        at each point i, include positive number or negative current number
-        subproblem: from point i, with current sum, what is the no. of ways that the target can
-        be achieved
-        dp state: (i, currSum)
-        base case: 
-        check at last:
-            if i == len(nums)
-                if currSum == target, return 1
-                else, return 0
-        recursion: 
-        take pos: dfs(i+1, target+nums[i]), i+1 since at each i we need to take the number only once
-        take neg: dfs(i+1, target-nums[i])
-        add both
-        TC: O(len of nums * sum of nums(all possible values of sum))
-        SC: same as TC
+        space optimized version
         '''
-        memo = {}
-        def dfs(i, currSum):
-            if i >= len(nums):
-                if currSum == target:
-                    return 1
-                else:
-                    return 0
-            if (i,currSum) in memo:
-                return memo[(i,currSum)]
-            
-            ways = dfs(i+1, currSum+nums[i]) + dfs(i+1, currSum-nums[i])
 
-            memo[(i,currSum)] = ways
-
-            return ways
+        currentRow = defaultdict(int)
+        currentRow[0] = 1 # for getting sum 0, there is 1 way
+        for i in range(len(nums)):
+            nextRow = defaultdict(int)
+            for currSum, ways in currentRow.items():
+                nextRow[currSum+nums[i]] += ways
+                nextRow[currSum-nums[i]] += ways
+            currentRow = nextRow
         
-        return dfs(0,0)
-
-
+        return currentRow[target]
