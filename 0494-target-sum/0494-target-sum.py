06c6c742df1499db9d6ffac3bf4f4223751a68ea
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2025-01-02 12:19:58 -0500

    Time: 182 ms (36.52%), Space: 38.5 MB (27.39%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -7,8 +7,11 @@ class Solution:
         subproblem: from point i, with current sum, what is the no. of ways that the target can
         be achieved
         dp state: (i, currSum)
-        base case: if currSum == target, return 1
-        if i>len(nums), return 0
+        base case: 
+        check at last:
+            if i == len(nums)
+                if currSum == target, return 1
+                else, return 0
         recursion: 
         take pos: dfs(i+1, target+nums[i]), i+1 since at each i we need to take the number only once
         take neg: dfs(i+1, target-nums[i])
