161ce7e28e6b9a171c4cd8933a1ff893e81641b9
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2025-01-02 19:32:03 -0500

    Time: 7 ms (1.85%), Space: 18.3 MB (6.78%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,24 +1,17 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        '''
-        dfs tree from (0,0) to (m-1,n-1)
-        dp[(r,c)] = count the number of ways to reach destination from here
-
-        subproblem; at each point in array, there are 2 possible paths, down and right
-        dfs(r,c)
-        '''
-
-        ways = 0
         memo = {}
         def dfs(r,c):
+            if (r not in range(m) or 
+                c not in range(n)):
+                return 0
             if (r,c) == (m-1,n-1):
                 return 1
-            if (r<0 or r>=m or c<0 or c>=n):
-                return 0
             if (r,c) in memo:
                 return memo[(r,c)]
-            ways = dfs(r+1,c) + dfs(r,c+1)
+            
+            ways = dfs(r+1,c) +dfs(r,c+1)
             memo[(r,c)] = ways
             return ways
-        
         return dfs(0,0)
+
