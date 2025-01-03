5fa9a85fbf9b3a71c171dae9b61bba439bb636a3
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2025-01-02 21:18:27 -0500

    Time: 247 ms (15.24%), Space: 22.5 MB (22.6%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,36 +1,25 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        '''
-        recursive function: longest path: from this point r,c what is the longest path i can travel to
-
-        base case: if can't traverse to any of the neighbors, return 0
-
-        dp state, r,c : longest path that can be traversed from this point
-        the next call would be only made if the val is increasing. So the possibility of revisiting a node vanishes with that check
-        '''
-        path=set()
+        dp = {}
         M = len(matrix)
         N = len(matrix[0])
-        neighbors = [[0,1],[0,-1],[1,0],[-1,0]]
-        memo = {}
+        n = [[1,0],[-1,0],[0,1],[0,-1]]
         def dfs(r,c):
-
-            # if (r not in range(M) or
-            #     c not in range(N)):
-            #     return 0
-            if (r,c) in memo:
-                return memo[(r,c)]
-            #traverse to neighbors
+            
+            if (r,c) in dp:
+                return dp[(r,c)]
             longestPath = 1
-            for dr, dc in neighbors:
-                nr, nc = dr+r, dc + c
-                if (0<=nr<M and 0<=nc<N and 
-                    matrix[nr][nc]>matrix[r][c]):
-                    longestPath = max(longestPath,1+dfs(nr,nc))
-            memo[(r,c)] = longestPath
+            # neighbors
+            for dr, dc in n:
+                nr, nc = dr+r, dc+c
+                if (nr in range(M) and 
+                    nc in range(N) and
+                    matrix[r][c]<matrix[nr][nc]):
+                    longestPath = max(longestPath, 1+dfs(nr,nc))
+            dp[(r,c)] = longestPath
             return longestPath
-        lp = 0
         for r in range(M):
             for c in range(N):
-                lp = max(lp,dfs(r,c))
-        return lp
+                dfs(r,c)
+        return max(dp.values())
+                
