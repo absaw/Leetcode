19cfee7442f4e71dbd6d7e5a36160238a57f9081
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2025-01-02 20:19:24 -0500

    Time: 1226 ms (6.06%), Space: 274.4 MB (7.26%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,19 +1,36 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = {}
-        def dfs(i, currSum):
-            if currSum==amount:
-                return 1
-            if i>=len(coins) or currSum > amount:
-                return 0
-            if (i,currSum) in dp:
-                return dp[(i,currSum)]
-            
-            # pick this coin
-            ways=dfs(i, currSum+coins[i])
-            #don't pick this coin
-            ways += dfs(i+1, currSum)
-            dp[(i,currSum)] = ways
-            return ways
-        return dfs(0, 0)
+        
+        '''
+        recursion subproblem: with given i, currSum, in how many ways cna i reach the
+        amount
+        param(i, currSum)
+        basecase : i>=len(coins) return 0
+        currSum == amount: return 1
+        currSum >amount return 0
 
+        '''
+        memo = {}
+
+        def dfs(i, currSum):
+
+            if i>=len(coins) or currSum>amount:
+                return 0
+            
+            if currSum == amount:
+                return 1
+            
+            if (i,currSum) in memo:
+                return memo[(i,currSum)]
+            
+            # solve
+
+            ways = 0
+
+            ways = dfs(i, currSum+coins[i]) + dfs(i+1,currSum)
+
+            memo[(i,currSum)] = ways
+
+            return ways
+        
+        return dfs(0,0)
