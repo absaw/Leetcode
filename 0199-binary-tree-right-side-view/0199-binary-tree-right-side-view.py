bc294423cbc6f2886a48d53928a188bdcf8bee16
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2025-01-03 12:53:57 -0500

    Time: 0 ms (100%), Space: 17.9 MB (8.23%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -4,38 +4,18 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-import collections
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
         if not root:
             return []
-        res = []
-        ll = []
-        queue = collections.deque()
-        curr_level = -1
-
-        queue.append([root,curr_level+1])
-
-        while queue:
-
-            node, level = queue.popleft()
-
-            if level == curr_level:
-                ll.append(node.val)
-
-            else:
-                if ll:
-                    res.append(ll[-1])
-                ll = [node.val]
-                curr_level += 1
-            
-            for neighbor in [node.left,node.right]:
-                if neighbor:
-                    queue.append([neighbor,level+1])
-        res.append(ll[-1])
-        
+        def dfs(node,level):
+            if level==len(res):
+                res.append([])
+            res[level] = node.val
+            if node.left:
+                dfs(node.left,level+1)
+            if node.right:
+                dfs(node.right,level+1)
+        dfs(root,0)
         return res
-
-
-        
-
