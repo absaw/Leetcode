bb7ffd1334f5f32b74e2080aad0fa41927892843
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2025-01-04 17:04:47 -0500

    Time: 3108 ms (13.63%), Space: 153.6 MB (28.86%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
@@ -1,49 +1,38 @@
-'''
-Min cost spanning tree is asked
-Make adj list for each number, for all other points which can be its neighbors
-
-Prim's algorithm which is simliar to dijstra. Maintain a minHeap to track the min next neighbor which can be visited.
-add any element to the minHeap
-start while loop
-pop out element. If element is visited, then continue
-add it to visited set. add the cost to result.
-visit neighbors which are adjacent to the heap.
-
-'''
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         
-        # create the adj list for all pair of points
-
+        #label for each point is 0-indexed from 0 to n-1
+        adjList = collections.defaultdict(list) # (x,y): [(edge_length, neighbor)]
         N = len(points)
-
-        adjList = collections.defaultdict(list)# cost, point_index
-
         for i in range(N):
             x1,y1 = points[i]
             for j in range(i+1,N):
-                x2, y2 = points[j]
-                cost = abs(y1-y2) + abs(x1-x2)
+                x2,y2 = points[j]
 
-                adjList[i].append([cost,j])
-                adjList[j].append([cost,i])
+                cost = abs(y2-y1) + abs(x2-x1)
+                adjList[i].append((cost,j))
+                adjList[j].append((cost,i))
         
-        # prim's algo
-        import heapq
-        visited = set()
-        minHeap = [(0,0)] # cost, node
-        totalCost = 0
+        #prim's
+        visited=set()
+        minHeap = [(0,0)] #(cost, point)
+        minCost = 0
+        
         while minHeap:
+            
+            cost, point = heapq.heappop(minHeap)
 
-            cost, node = heapq.heappop(minHeap)
-            if node in visited:
+            if point in visited:
                 continue
-            totalCost += cost
-            visited.add(node)
-            # neighbors
+            
+            minCost += cost
+            visited.add(point)
 
-            for nCost, neighbor in adjList[node]:
+            # add all neigboring edges to heap
+
+            for edge_length, neighbor in adjList[point]:
                 if neighbor not in visited:
-                    heapq.heappush(minHeap, (nCost,neighbor))
+                    heapq.heappush(minHeap,(edge_length,neighbor))
+            
+        return minCost
 
-        return totalCost
