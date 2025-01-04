6b583bef736a6f2446f2e2d4302a5e9d62ca1ed2
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2025-01-04 14:50:03 -0500

    Time: 374 ms (67.39%), Space: 20.3 MB (6.2%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -1,43 +1,26 @@
-'''
-Dijstra's algo
-Basically weighted bfs. But instead of a queue, I am using minHeap, to maintain the 
-nodes to visit next. at each point i pop the min weight neighbor
-if node already visited, then continue
-add it to visited.
-update times value to be max
-Then update the weights of all neighbors of this node
-
-'''
-import heapq
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         
-        # create adj List 
-        adjList = collections.defaultdict(list)
-
-        for u, v, w in times:
-            adjList[u].append((v,w))
-        # 1:(1,2)
-        minHeap = [(0,k)]
+        adjList = {i:[] for i in range(1,n+1)}
+        
+        for u,v,w in times:
+            adjList[u].append([v,w])
+        
+        minHeap = [[0,k]]
         visited = set()
-        heapq.heapify(minHeap)
-        res = 0
+        # visited.add(k)
+        minTime = 0
         while minHeap:
 
-            weight, node = heapq.heappop(minHeap)
+            dist, node = heapq.heappop(minHeap)
             if node in visited:
                 continue
-            
+            minTime = max(minTime, dist)
             visited.add(node)
-            res = max(res,weight)
-            # traverse through neighbors and update the weights
-
-            for neighbor, n_weight in adjList[node]:
+            # add neighbors to heap
+            for neighbor, edge in adjList[node]:
                 if neighbor not in visited:
-                    heapq.heappush(minHeap,(n_weight+weight, neighbor))
-            
-        return res if len(visited) == n else -1
-
-
-        
-
+                    heapq.heappush(minHeap,[dist+edge,neighbor])
+            if len(visited)==n:
+                return minTime
+        return -1
