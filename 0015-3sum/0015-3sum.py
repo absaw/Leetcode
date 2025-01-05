66171eaf9be5a5e890f6cc5af08abdeeae26748e
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2025-01-05 15:45:22 -0500

    Time: 522 ms (77.62%), Space: 20.5 MB (38.71%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -7,8 +7,8 @@ class Solution:
 
         for i, num in enumerate(nums):
 
-            if num >0:
-                break
+            # if num >0:
+            #     break
             if i>0 and nums[i-1] == nums[i]:
                 continue
             
