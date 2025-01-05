d3e536c0896545d3105d1deb980ec6caaa999e0f
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2025-01-05 15:01:48 -0500

    Time: 1 ms (89.64%), Space: 17.7 MB (31.24%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,22 +1,29 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = defaultdict(set)
-        cols = defaultdict(set)
-        boxes = defaultdict(set)
+        
+        rowDict={i:set() for i in range(9)}
+        colDict={i:set() for i in range(9)}
+        boxDict={}
 
+        for r in range(3):
+            for c in range(3):
+                boxDict[(r,c)] = set()
+            
         for r in range(9):
             for c in range(9):
-
-                if board[r][c] == ".":
+                val = board[r][c]
+                if val == ".":
                     continue
-                
-                if (board[r][c] in rows[r] or
-                    board[r][c] in cols[c] or 
-                    board[r][c] in boxes[(r//3,c//3)]):
+                if val in rowDict[r]:
                     return False
-                
-                rows[r].add(board[r][c])
-                cols[c].add(board[r][c])
-                boxes[(r//3,c//3)].add(board[r][c])
-        
+                rowDict[r].add(val)
+
+                if val in colDict[c]:
+                    return False
+                colDict[c].add(val)
+
+                if val in boxDict[(r//3,c//3)]:
+                    return False
+                boxDict[(r//3,c//3)].add(val)
+
         return True
