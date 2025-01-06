0fbab3cd8008cd248d532d65d0d4d6357d36e075
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2025-01-05 19:09:48 -0500

    Time: 127 ms (36.33%), Space: 33.6 MB (20.78%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,19 +1,23 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         
-        # question of monotonic DECREASING stack, since the stack only holds
-        # values in decreasing order 
+        stack = [] # append (val,index)
+        result = [0]*len(temperatures)
+        for i,temp in enumerate(temperatures):
+            if not stack:
+                stack.append((i,temp))
+                continue
+            
+            while stack and stack[-1][1] < temp:
+                i_top, t_top = stack.pop()
+                result[i_top] = i - i_top
+            
+            stack.append((i,temp))
+        
+        return result
 
-        stack = []
-        answer = [0]* len(temperatures)
 
-        for i, temp in enumerate(temperatures):
 
-            while stack and stack[-1][0]<temp:
-                topElement, topIndex = stack.pop()
-                answer[topIndex] = i - topIndex
 
-            stack.append([temp,i])
-        return answer
 
-                
+
