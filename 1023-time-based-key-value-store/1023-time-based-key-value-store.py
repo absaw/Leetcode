6ba42d0e0f7146702958e784f4626844a41635ce
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2025-01-05 20:55:10 -0500

    Time: 131 ms (49.37%), Space: 74.2 MB (53.03%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.py
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -1,41 +1,33 @@
 class TimeMap:
 
     def __init__(self):
-        self.dict = collections.defaultdict(list)
-        '''
-        {
-            foo: [[foo,bar],[1,3]]
-        }
-        
-        '''
+        self.timeDict = collections.defaultdict(list)
+
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.dict:
-
-            self.dict[key].append([value])
-            self.dict[key].append([timestamp])
-        else:
-            self.dict[key][0].append(value)
-            self.dict[key][1].append(timestamp)
+        self.timeDict[key].append((timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
-        if key not in self.dict:
+        
+        if key not in self.timeDict:
             return ""
-        vals = self.dict[key][0]
-        nums = self.dict[key][1]
-        # [ 1,2,4,5,6,7,9,10]
-
+        times = self.timeDict[key]
         l = 0
-        r = len(nums)-1
+        r = len(times)-1
         ans = -1
         while l<=r:
             mid = l + (r-l)//2
-            if nums[mid] <= timestamp:
+
+            if times[mid][0] == timestamp:
+                ans = mid
+                break
+            if times[mid][0]< timestamp:
                 ans = mid
                 l = mid+1
             else:
                 r = mid-1
-        return vals[ans] if ans>-1 else ""
+        
+        return times[ans][1] if ans != -1 else ""
 
 
 
