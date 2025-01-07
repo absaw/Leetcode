dce83af030f5ef0b3da4ec26f38170244558cd7a
Abhishek Sawalkar <34976856+absaw@users.noreply.github.com>
2025-01-07 13:22:53 -0500

    Time: 1185 ms (7.99%), Space: 274.4 MB (7.21%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,33 +1,20 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         
-        '''
-        recursion subproblem: with given i, currSum, in how many ways cna i reach the
-        amount
-        param(i, currSum)
-        basecase : i>=len(coins) return 0
-        currSum == amount: return 1
-        currSum >amount return 0
-
-        '''
         memo = {}
 
         def dfs(i, currSum):
-
-            if i>=len(coins) or currSum>amount:
-                return 0
-            
             if currSum == amount:
                 return 1
+            
+            #invalid case
+            if i >= len(coins) or currSum > amount:
+                return 0
             
             if (i,currSum) in memo:
                 return memo[(i,currSum)]
             
-            # solve
-
-            ways = 0
-
-            ways = dfs(i, currSum+coins[i]) + dfs(i+1,currSum)
+            ways = dfs(i, currSum + coins[i]) + dfs(i+1, currSum)
 
             memo[(i,currSum)] = ways
 
